cd07a52fad76a79b54bcca15e812d463bbccb7a9
2mdsajid <2mdsajid@gmail.com>
2023-04-11 21:19:52 +0545

    cors

--- app.js
+++ app.js
@@ -21,6 +21,15 @@ app.use('/public', express.static('public'));
 let cors = require('cors')
 app.use(cors());
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+
 //before AUTH.JS loading so that it effects
 app.use(express.json())
 
